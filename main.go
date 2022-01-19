9feee07e79f983ecf30bf19eaf51c22c94591415
Pac <pac@pa0x43.me>
2022-01-19 23:04:51 +0100

    Added main workflow

--- main.go
+++ main.go
@@ -3,41 +3,43 @@ package main
 import (
 	"main/database"
 	"main/pr0gramm"
+	"main/recognition"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
-type Pr0Service struct {
+type SauceSession struct {
 	Session *pr0gramm.Session
-	After   pr0gramm.Timestamp
+	DB      *database.DB
 }
-
-var db *database.Session
 
 func main() {
 	godotenv.Load()
-	_, err := database.Connect()
+	db, err := database.Connect()
 	if err != nil {
 		log.WithError(err).Error("Error while connecting to a database!")
 	}
-	/* _ = recognition.DetectMusic("https://vid.pr0gramm.com/2022/01/19/388fb24da04c528c.mp4") */
-	/* session := pr0gramm.NewSession(http.Client{Timeout: 10 * time.Second})
+	session := pr0gramm.NewSession(http.Client{Timeout: 10 * time.Second})
 	if resp, err := session.Login(os.Getenv("PR0_USER"), os.Getenv("PR0_PASSWORD")); err != nil {
-		log.WithError(err).Fatal("Could not login into pr0gramm!")
+		log.WithError(err).Fatal("Error logging in to pr0gramm")
 		return
 	} else {
 		if !resp.Success {
-			log.WithError(err).Fatal("Could not login into pr0gramm!")
+			log.Fatal("Error logging in to pr0gramm")
 			return
 		}
 	}
-	pr0 := Pr0Service{Session: session, After: pr0gramm.Timestamp{time.Unix(1623837600, 0)}}
-	DoEvery(5*time.Second, pr0.ticker) */
+	pr0 := SauceSession{Session: session, DB: db}
+	DoEvery(5*time.Second, pr0.ticker)
 }
 
-/* func (pr0 *Pr0Service) ticker(t time.Time) {
-	msgarr, err := pr0.Session.GetComments()
+func (sauce *SauceSession) ticker(t time.Time) {
+	msgarr, err := sauce.Session.GetComments()
 	if err != nil {
 		log.WithError(err)
 	}
@@ -46,9 +48,30 @@ func main() {
 		if msg.Read != 1 {
 			// Ungelesene Kommentare
 			if strings.Contains(strings.ToLower(msg.Message), "@sauce") {
-				// Bot wurde Markiert!
+				// Bot wurde Markiert
+				log.Debug("Bot was marked by %s", msg.Name)
+				var item database.Items
+				sauce.DB.Find(&item, "item_id", msg.ItemID)
+				if item.ItemID > 0 {
+					//Post ist in der Datenbank
+					if len(item.Title) > 0 {
+						// Es ist ein Titel vorhanden, sende Nachricht mit den Metadaten
+						log.WithFields(log.Fields{"item_id": item.ID}).Debug("Post has already been queried, information available")
+					} else {
+						// es ist kein Titel vorhanden, sende Nachricht ohne Metadaten
+						log.WithFields(log.Fields{"item_id": item.ID}).Debug("Post has already been queried, no information available")
+					}
+				} else {
+					// Post ist nicht in der Datenbank
+					log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("Post has never been queried, searching for the music")
+					sourceURL, err := ResolveThumb(msg.Thumb)
+					if err != nil {
+						log.WithError(err).Error("Could not verify the video URL of the post")
+					} else {
+						meta = recognition.DetectMusic(sourceURL)
+					}
+				}
 			}
 		}
 	}
 }
-*/
