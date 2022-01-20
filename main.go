cb7506b6bf2ac6634b6b3c0ea11b1cbf36fe2e63
Pac <pac@pa0x43.me>
2022-01-20 16:02:00 +0100

    Added .env and main code

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"main/recognition"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -17,10 +19,12 @@ import (
 type SauceSession struct {
 	Pr0 *pr0gramm.Session
 	DB  *database.DB
+	WG  sync.WaitGroup
 }
 
 func main() {
 	godotenv.Load()
+	log.SetLevel(log.DebugLevel)
 	db, err := database.Connect()
 	if err != nil {
 		log.WithError(err).Error("Error while connecting to a database!")
@@ -41,6 +45,7 @@ func main() {
 
 func (sauce *SauceSession) ticker(t time.Time) {
 	msgarr, err := sauce.Pr0.GetComments()
+	log.Debug("Check Pr0gramm Comments")
 	if err != nil {
 		log.WithError(err)
 	}
@@ -50,81 +55,112 @@ func (sauce *SauceSession) ticker(t time.Time) {
 			// Ungelesene Kommentare
 			if strings.Contains(strings.ToLower(msg.Message), "@sauce") {
 				// Bot wurde Markiert
-				log.Debug("Bot was marked by %s", msg.Name)
-				var item database.Items
-				sauce.DB.Find(&item, "item_id", msg.ItemID)
-				if item.ItemID > 0 {
-					//Post ist in der Datenbank
-					if len(item.Title) > 0 {
-						// Es ist ein Titel vorhanden, sende Nachricht mit den Metadaten
-						log.WithFields(log.Fields{"item_id": item.ID}).Debug("Post has already been queried, information available")
-						message := fmt.Sprintf("Hallo %s,\n\nDu hast bei https://pr0gramm.com/new/%d nach der Musik gefragt.\nJemand hat bereits danach gefragt, daher erhälst du hier nur eine Kopie.\n\nTitel: %s\nAlbum: %s\nArtist: %s\n\nHier ist ein Link: %s",
-							msg.Name,
-							msg.ItemID,
-							item.Title,
-							item.Album,
-							item.Artist,
-							item.Url,
-						)
-						_, err := sauce.Pr0.SendMessage(msg.Name, message)
-						if err != nil {
-							log.WithError(err).Error("Could not send private message to user")
-						} else {
-							log.Info("Private message sent to user")
-						}
-					} else {
-						// es ist kein Titel vorhanden, sende Nachricht ohne Metadaten
-						log.WithFields(log.Fields{"item_id": item.ID}).Debug("Post has already been queried, no information available")
-						message := fmt.Sprintf("Hallo %s,\n\nDu hast bei https://pr0gramm.com/new/%d nach der Musik gefragt.\nLeider wurden keine Informationen gefunden.",
-							msg.Name,
-							msg.ItemID,
-						)
-						_, err := sauce.Pr0.SendMessage(msg.Name, message)
-						if err != nil {
-							log.WithError(err).Error("Could not send private message to user")
-						} else {
-							log.Info("Private message sent to user")
-						}
-					}
+				log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug(fmt.Sprintf("Bot was marked by %s", msg.Name))
+				sauce.WG.Add(1)
+				go sauce.detect(msg)
+			}
+		}
+	}
+	sauce.WG.Wait()
+}
+
+func (sauce *SauceSession) detect(msg pr0gramm.Message) {
+	var item database.Items
+	sauce.DB.Find(&item, "item_id", msg.ItemID)
+	if item.ItemID > 0 {
+		//Post ist in der Datenbank
+		if len(item.Title) > 0 {
+			// Es ist ein Titel vorhanden, sende Nachricht mit den Metadaten
+			log.WithFields(log.Fields{"item_id": item.ItemID}).Debug("Post has already been queried, information available")
+			message := fmt.Sprintf("Hallo %s,\n\nDu hast bei https://pr0gramm.com/new/%d nach der Musik gefragt.\nJemand hat bereits danach gefragt, daher erhälst du hier nur eine Kopie.\n\nTitel: %s\nAlbum: %s\nArtist: %s\n\nHier ist ein Link: %s",
+				msg.Name,
+				msg.ItemID,
+				item.Title,
+				item.Album,
+				item.Artist,
+				item.Url,
+			)
+			_, err := sauce.Pr0.SendMessage(msg.Name, message)
+			if err != nil {
+				log.WithError(err).Error("Could not send private message to user")
+			} else {
+				log.Info("Private message sent to user")
+			}
+		} else {
+			// es ist kein Titel vorhanden, sende Nachricht ohne Metadaten
+			log.WithFields(log.Fields{"item_id": item.ID}).Debug("Post has already been queried, no information available")
+			message := fmt.Sprintf("Hallo %s,\n\nDu hast bei https://pr0gramm.com/new/%d nach der Musik gefragt.\nLeider wurden keine Informationen gefunden.",
+				msg.Name,
+				msg.ItemID,
+			)
+			_, err := sauce.Pr0.SendMessage(msg.Name, message)
+			if err != nil {
+				log.WithError(err).Error("Could not send private message to user")
+			} else {
+				log.Info("Private message sent to user")
+			}
+		}
+	} else {
+		// Post ist nicht in der Datenbank
+		log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("Post has never been queried, searching for the music")
+		sourceURL, err := ResolveThumb(msg.Thumb)
+		if err != nil {
+			log.WithError(err).Error("Could not verify the video URL of the post")
+		} else {
+			dt := time.Now()
+			meta := recognition.DetectMusic(sourceURL)
+			if len(meta.Title) > 0 {
+				// Metadaten gefunden
+				log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("Metadata was found")
+				dbItem := database.Items{
+					ItemID: msg.ItemID,
+					Title:  meta.Title,
+					Album:  meta.Album,
+					Artist: meta.Artist,
+					Url:    meta.Url,
+					Metadata: database.Metadata{
+						SpotifyURL: meta.Links.Spotify,
+						SpotifyID:  meta.IDS.Spotify,
+						DeezerURL:  meta.Links.Deezer,
+						DeezerID:   strconv.Itoa(meta.IDS.Deezer),
+					},
+				}
+				result := sauce.DB.Create(&dbItem)
+				if result.Error != nil {
+					log.WithError(result.Error).Error("Error saving metadata to the database!")
+				}
+				message := fmt.Sprintf("Es wurden folgende Informationen dazu gefunden:\n%s - %s\nAus dem Album: %s\n\nHier ist ein Link: %s\nZeitpunkt der Überprüfung %s",
+					meta.Title,
+					meta.Album,
+					meta.Album,
+					meta.Url,
+					dt.Format("02.01.2006 um 15:04"),
+				)
+				_, err := sauce.Pr0.PostComment(msg.ItemID, message, msg.ID)
+				if err != nil {
+					log.WithError(err).Error("Could not post comment")
 				} else {
-					// Post ist nicht in der Datenbank
-					log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("Post has never been queried, searching for the music")
-					sourceURL, err := ResolveThumb(msg.Thumb)
-					if err != nil {
-						log.WithError(err).Error("Could not verify the video URL of the post")
-					} else {
-						dt := time.Now()
-						meta := recognition.DetectMusic(sourceURL)
-						if len(meta.Title) > 0 {
-							// Metadaten gefunden
-							log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("Metadata was found")
-							message := fmt.Sprintf("Es wurden folgende Informationen dazu gefunden:\n%s - %sAus dem Album: %s\n\nHier ist ein Link: %s\nZeitpunkt der Überprüfung %s",
-								meta.Title,
-								meta.Album,
-								meta.Album,
-								meta.Url,
-								dt.Format("02.01.2006 um 15:04"),
-							)
-							_, err := sauce.Pr0.PostComment(msg.ItemID, message, msg.ID)
-							if err != nil {
-								log.WithError(err).Error("Could not post comment")
-							} else {
-								log.Info("Comment written")
-							}
-						} else {
-							// Keine Metadaten gefunden
-							log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("No metadata found")
-							message := fmt.Sprintf("Es wurden keine Informationen zu dem Lied gefunden\n\nZeitpunkt der Überprüfung %s", dt.Format("02.01.2006 um 15:04"))
-							_, err := sauce.Pr0.PostComment(msg.ItemID, message, msg.ID)
-							if err != nil {
-								log.WithError(err).Error("Could not post comment")
-							} else {
-								log.Info("Comment written")
-							}
-						}
-					}
+					log.Info("Comment written")
+				}
+			} else {
+				// Keine Metadaten gefunden
+				log.WithFields(log.Fields{"item_id": msg.ItemID}).Debug("No metadata found")
+				message := fmt.Sprintf("Es wurden keine Informationen zu dem Lied gefunden\n\nZeitpunkt der Überprüfung %s", dt.Format("02.01.2006 um 15:04"))
+				dbItem := database.Items{
+					ItemID: msg.ItemID,
+				}
+				result := sauce.DB.Create(&dbItem)
+				if result.Error != nil {
+					log.WithError(result.Error).Error("Error saving metadata to the database!")
+				}
+				_, err := sauce.Pr0.PostComment(msg.ItemID, message, msg.ID)
+				if err != nil {
+					log.WithError(err).Error("Could not post comment")
+				} else {
+					log.Info("Comment written")
 				}
 			}
 		}
 	}
+	sauce.WG.Done()
 }
